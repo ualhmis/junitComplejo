7f41a7725bc167aaa54e9f4f1a0aa2d30dd3bbd7
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2021-03-05 14:51:42 +0100

    junit5 test suite

---
+++ Junit5Complejo/test/junitenclase/AllTest.java
@@ -0,0 +1,14 @@
+package junitenclase;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("JUnit Test Suite")
+@SelectPackages("junitenclase")
+public class AllTest {
+}
+
