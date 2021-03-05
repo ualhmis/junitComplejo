a7125f3e139777ede4fc430d213c354a38905f13
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2021-03-05 14:05:55 +0100

    JUnit5Complejo project

---
+++ Junit5Complejo/test/junitenclase/TestSumaRestaComplejos.java
@@ -0,0 +1,47 @@
+package junitenclase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import junitComplejo.Complejo;
+
+class TestSumaRestaComplejos {
+	
+	@ParameterizedTest (name = "{index} => sumar ({0},{1}) con ({2},{3}) sale ({4},{5})")
+	@CsvSource({"  3,  5,  1, -1,  4,  4 ", 
+			    "  3,  5,  1,  1,  4,  6 ", 
+			    " -3,  5, -1,  1, -1,  6 ", 
+			    " -3, -5, -1,  1, -4, -3 ", 
+			    " -3, -5, -1, -1, -4, -6 "})
+	
+	void testsuma(int c1_pr, int c1_pi, int c2_pr, int c2_pi, int exp_pr, int exp_pi){
+		// 1. Arrange 
+        Complejo c1 = new Complejo(c1_pr, c1_pi);
+        Complejo c2 = new Complejo(c2_pr, c2_pi);
+        // 2. Act
+        Complejo resultado = c1.sumar(c2);
+        // 3. Assert
+        assertEquals(exp_pr, resultado.getParteReal(), 0.0000001, "Falla la parte real");
+        assertEquals(exp_pi, resultado.getParteImaginaria(), 0.0000001, "Falla la parte imaginaria");
+	}
+
+	
+	@ParameterizedTest (name = "{index} => restar ({0},{1}) menos ({2},{3}) sale ({4},{5})")
+	@CsvFileSource(files = "test/junitenclase/datosResta.csv")
+	void testresta(int c1_pr, int c1_pi, int c2_pr, int c2_pi, int exp_pr, int exp_pi){
+		// 1. Arrange 
+        Complejo c1 = new Complejo(c1_pr, c1_pi);
+        Complejo c2 = new Complejo(c2_pr, c2_pi);
+        // 2. Act
+        Complejo resultado = c1.restar(c2);
+        // 3. Assert: assertAll comprueba varios asserts (JUnit 5)
+        assertAll(
+            () -> assertEquals(exp_pr, resultado.getParteReal(), 0.0000001, "Falla la parte real"),
+            () -> assertEquals(exp_pi, resultado.getParteImaginaria(), 0.0000001, "Falla la parte imaginaria")
+        );
+	}
+
+}
---
+++ junitComplejo/test/junitComplejo/AllTests2.java
@@ -0,0 +1,11 @@
+package junitComplejo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ComplejoTest.class, ComplejoTest2.class })
+public class AllTests2 {
+
+}
